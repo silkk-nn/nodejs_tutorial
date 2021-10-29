c25d7d30556fcb821122e3fc4738fe02bb175855
silkk-nn <70426262+silkk-nn@users.noreply.github.com>
2021-10-30 01:35:49 +0700

    start

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 4s linear infinite',
+       },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
